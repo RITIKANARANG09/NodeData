d21468d0124ee17106cc1650bb30e7aec26c6551
ritika_narang <ritsnarang09@gmail.com>
2023-04-29 23:49:43 +0530

    initial commit

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,107 @@
+import { useState, useEffect } from "react";
+import UsersTable from "./UsersTable";
+import CityTable from "./CityTable";
+import axios from "axios";
+export default function Home() {
+  //{"first_name":"","last_name":"","email":"","gender":"","income":"","city":"","car":"","quote":"","phone_price":""}
+  const [data, setData] = useState([]);
+  const [showCityTable, setShow] = useState(false);
+
+  function handleClick(url) {
+    setData([]);
+    if (url === "five") setShow(true);
+    else setShow(false);
+    axios
+      .get("http://localhost:3001/" + url)
+      .then((response) => {
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "darkblue",
+          padding: "14px",
+          height: "14px",
+          textAlign: "center",
+        }}
+      >
+        <button
+          style={{
+            border: "0px",
+            backgroundColor: "darkblue",
+            marginRight: "20px",
+            fontSize: "14px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={() => handleClick("one")}
+        >
+          One
+        </button>
+        <button
+          id="1"
+          /* onDoubleClick={()=>handledblClick(button)}*/ style={{
+            border: "0px",
+            backgroundColor: "darkblue",
+            marginRight: "20px",
+            fontSize: "14px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={() => handleClick("two")}
+        >
+          Two
+        </button>
+        <button
+          style={{
+            border: "0px",
+            backgroundColor: "darkblue",
+            marginRight: "20px",
+            fontSize: "14px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={() => handleClick("three")}
+        >
+          Three
+        </button>
+        <button
+          style={{
+            border: "0px",
+            backgroundColor: "darkblue",
+            marginRight: "20px",
+            fontSize: "14px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={() => handleClick("four")}
+        >
+          Four
+        </button>
+        <button
+          style={{
+            border: "0px",
+            backgroundColor: "darkblue",
+            fontSize: "14px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={() => handleClick("five")}
+        >
+          Five
+        </button>
+      </div>
+      {showCityTable ? (
+        <CityTable cities={data} />
+      ) : (
+        <UsersTable users={data} />
+      )}
+    </>
+  );
+}
